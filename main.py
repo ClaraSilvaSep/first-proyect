6ed3e67c465127538844ba100cb7b899176e93f0
Clara Silva Sepúlveda <clara.silva.sepulveda.99@gmail.com>
2023-04-11 21:23:25 +0200

    test file ad ded

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ print('Hello World')
 #si queremos sólo subir un fichero (en este caso solo tengo uno que es main.py) se escribe 'git add main.py'
 #el comando git commit necesita un parametro, por lo que se deja algo entre comillas --> se deja entre comillas el motivo del commit en inglés
 print('Esto es un codigo que rompe todo')
-
-
-
+#con git bran el codigo dice en qué rama estamos, sale master y no main pq no la hemos cambiado
+#si queremos crear otra rama ponemos el codigo git branch develop
+#si vuelvo a poner git branch aparece con asterisco que sigo en la rama main-master pero aparece la develop
+#para cambiar de rama poner git checkout develop
+#con git branch me aparece que ahora estamos en la rama develop
+#cuando se hace git branch develop se compian todos los cambios
+#con git merge se funde-fusiona la rama develop con main
 
 
