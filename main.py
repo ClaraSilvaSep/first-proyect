8378fdc9dd30abd9dd1ac211d7ac88a9ac09c4cc
Clara Silva Sepúlveda <clara.silva.sepulveda.99@gmail.com>
2023-04-11 20:51:10 +0200

    gitignore

---
+++ main.py
@@ -0,0 +1,13 @@
+#primero se manda la bolita blanca con 'control s'
+#si tengo python instalado se imprime Hello World
+print('Hello World')
+#ahora en el working tenemos un fichero
+#si usamos el comando 'git add .' se suben todos los ficheros 
+#si queremos sólo subir un fichero (en este caso solo tengo uno que es main.py) se escribe 'git add main.py'
+#el comando git commit necesita un parametro, por lo que se deja algo entre comillas --> se deja entre comillas el motivo del commit en inglés
+print('Esto es un codigo que rompe todo')
+
+
+
+
+
